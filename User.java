81869d5dc01ab07b0da8ba656819d3ef9de09050
RyanTibbo87 <Ryan.Tibbo@keyin.com>
2025-02-28 06:05:58 -0330

    user file completed

--- User.java
+++ User.java
@@ -1,3 +1,22 @@
 public class User {
-    
+    String name;
+    TaskList taskList;
+
+    public User(String name) {
+        this.name = name;
+        this.taskList = new TaskList();
+    }
+
+    public void addTask(String description) {
+        taskList.addTask(description);
+    }
+
+    public void markTaskCompleted(String description) {
+        taskList.markTaskCompleted(description);
+    }
+
+    public void printTasks() {
+        System.out.println("Tasks for " + name + ":");
+        taskList.printTasks();
+    }
 }

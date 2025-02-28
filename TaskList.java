5251a2597750e0027fd21870035f7e4815e203ca
RyanTibbo87 <Ryan.Tibbo@keyin.com>
2025-02-28 06:03:10 -0330

    tasklist.java completed

--- TaskList.java
+++ TaskList.java
@@ -1,3 +1,35 @@
 public class TaskList {
-    
+    Task head;
+
+    public void addTask(String description) {
+        Task newTask = new Task(description);
+        if (head == null) {
+            head = newTask;
+        } else {
+            Task temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newTask;
+        }
+    }
+
+    public void markTaskCompleted(String description) {
+        Task temp = head;
+        while (temp != null) {
+            if (temp.description.equals(description)) {
+                temp.markCompleted();
+                return;
+            }
+            temp = temp.next;
+        }
+    }
+
+    public void printTasks() {
+        Task temp = head;
+        while (temp != null) {
+            System.out.println(temp);
+            temp = temp.next;
+        }
+    }
 }
